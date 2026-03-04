d1e441b2f4b88c5f2505e6842a9b6e77a7798746
raiai <raiai@10.76.234.172>
2026-03-04 11:15:10 +0000

    Finalised Task class

--- src/taskcli/Task.java
+++ src/taskcli/Task.java
@@ -7,6 +7,38 @@ public class Task {
 	private String createdAt;
 	private String updatedAt;
 	
+	public Task(int id, String description, String status, String createdAt, String updatedAt) {
+		this.id = id;
+		this.description = description;
+		this.status = status;
+		this.createdAt = createdAt;
+		this.updatedAt = createdAt;
+	}
 	
-
+	public int getID() {
+		return id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public String getCreatedAt() {
+		return createdAt;
+	}
+	public String getUpdatedAt() {
+		return updatedAt;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public void setUpdatedAt(String updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+ 
 }
