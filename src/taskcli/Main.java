18d22e92eefcf291ff9449cb7c47f51f6f07926f
Aidan <raiaidan06@gmail.com>
2026-03-17 14:21:58 +0000

    Completed all Main logic.

--- src/taskcli/Main.java
+++ src/taskcli/Main.java
@@ -1,5 +1,27 @@
 package taskcli;
 
 public class Main {
+	public static void main(String[] args) {
+		TaskManager taskManager = new TaskManager();
+		
+		if (args.length == 0) {
+			System.out.println("Please type a command");
+			return;
+		}
+		
+		if (args[0].equals("add")) {
+			taskManager.addTask(args[1]);
+		} else if (args[0].equals("delete")) {
+			taskManager.deleteTask(Integer.parseInt(args[1]));
+		} else if (args[0].equals("update")) {
+			taskManager.updateTask(Integer.parseInt(args[1]), args[2]);
+		} else if (args[0].equals("mark")) {
+			taskManager.markTask(Integer.parseInt(args[1]), args[2]);
+		} else if (args[0].equals("list")) {
+			taskManager.listTasks();
+		} else {
+			System.out.println("Unrecognised command");
+		}
+	}
 
 }
